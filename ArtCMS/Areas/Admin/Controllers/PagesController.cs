b28ad58b4a04db2c6af2f2d449c8c4155f303164
Robert Chou <robertchou91@gmail.com>
2019-07-10 11:15:34 +0800

    Pages View Changes

--- ArtCMS/Areas/Admin/Controllers/PagesController.cs
+++ ArtCMS/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArtCMS.Models.Data;
+using ArtCMS.Models.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,19 @@ namespace ArtCMS.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            // Declaring the list of PageVM
+            List<PageVM> pagesList;
+
+            
+            using (Db db = new Db())
+            {
+                // Init the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+            }
+
+            // Return the list
+            return View(pagesList);
         }
     }
 }
