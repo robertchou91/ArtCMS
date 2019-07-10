fd2eea5f8593094d1a570131199101a3853558cd
Robert Chou <robertchou91@gmail.com>
2019-07-10 17:05:34 +0800

    Last commit of the day.

--- ArtCMS/Areas/Admin/Controllers/PagesController.cs
+++ ArtCMS/Areas/Admin/Controllers/PagesController.cs
@@ -30,8 +30,26 @@ namespace ArtCMS.Areas.Admin.Controllers
 
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
+            return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+            }
             return View();
         }
     }
