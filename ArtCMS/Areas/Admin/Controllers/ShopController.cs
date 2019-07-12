3976ce55fe27877c418871eb25f07f0078435dad
Robert Chou <robertchou91@gmail.com>
2019-07-12 15:15:47 +0800

    Adding Adding, Sorting, and Deleting Actions to Categories View along with the Ajax to make these work.

--- ArtCMS/Areas/Admin/Controllers/ShopController.cs
+++ ArtCMS/Areas/Admin/Controllers/ShopController.cs
@@ -27,5 +27,81 @@ namespace ArtCMS.Areas.Admin.Controllers
             }
             return View(categoryVMList);
         }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            // Declare the id
+            string id;
+
+            using (Db db = new Db())
+            {
+                // check if category name is unique
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                // init DTO
+                var dto = new CategoryDTO();
+
+                // add to dto
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                db.Categories.Add(dto);
+                // save dto
+                db.SaveChanges();
+
+                //get the id
+                id = dto.Id.ToString();
+            }
+
+            // return the id
+            return id;
+        }
+
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // set initial count
+                int count = 1;
+
+                // declare CategoryDTO
+                CategoryDTO dto;
+
+                // set sorting for each category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        // DELETE: Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                // get the page
+                CategoryDTO dto = db.Categories.Find(id);
+
+                // remove the page
+                db.Categories.Remove(dto);
+
+                // Save
+                db.SaveChanges();
+            }
+
+            // Redirect
+            return RedirectToAction("Categories");
+        }
     }
 }
