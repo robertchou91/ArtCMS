95a2a4dbc61387a3379c665fa7ca0aba325864ef
Robert Chou <robertchou91@gmail.com>
2019-07-17 09:36:30 +0800

    Add Category Partial.

--- ArtCMS/App_Start/RouteConfig.cs
+++ ArtCMS/App_Start/RouteConfig.cs
@@ -12,6 +12,9 @@ namespace ArtCMS
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "ArtCMS.Controllers" });
+
             routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "ArtCMS.Controllers" });
             routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "ArtCMS.Controllers" });
             routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "ArtCMS.Controllers" });
---
+++ ArtCMS/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using ArtCMS.Models.Data;
+using ArtCMS.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtCMS.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Pages");
+        }
+
+        public ActionResult CategoryMenuPartial()
+        {
+            // declare the list of categoryvm
+            List<CategoryVM> categoryVMList;
+
+            // init the list
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+
+            // return partial view with list
+            return PartialView(categoryVMList);
+        }
+    }
+}
