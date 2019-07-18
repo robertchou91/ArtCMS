3d4da323ab0313d1f96a0dfe286629a0301de2cf
Robert Chou <robertchou91@gmail.com>
2019-07-18 16:25:46 +0800

    Add Account creation and was able to store the data in the localdb.

--- ArtCMS/Controllers/AccountController.cs
+++ ArtCMS/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArtCMS.Models.Data;
+using ArtCMS.Models.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -29,9 +31,72 @@ namespace ArtCMS.Controllers
 
         // GET: Account/create-account
         [ActionName("create-account")]
+        [HttpGet]
         public ActionResult CreateAccount()
         {
             return View("CreateAccount");
+        }
+
+        // POST: Account/create-account
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            // check model state
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            // check if password match
+            if (model.Password != model.ConfirmPassword)
+            {
+                ModelState.AddModelError("", "Password do not match!");
+                return View("CreateAccount", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // make sure username is unique
+                if (db.Users.Any(x => x.Username == model.Username))
+                {
+                    ModelState.AddModelError("", "Password do not match!");
+                    return View("CreateAccount", model);
+                }
+
+                // Create userDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                // add the DTO
+                db.Users.Add(userDTO);
+
+                // Save
+                db.SaveChanges();
+
+                // add UserRolesDTO
+                int id = userDTO.Id;
+
+                UserRoleDTO userRolesDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+
+                db.UserRoles.Add(userRolesDTO);
+                db.SaveChanges();
+            }
+
+            // Create Tempdata message
+            TempData["SM"] = "You are now registered and can log in!";
+
+            return Redirect("~/account/login");
         }
 
 
