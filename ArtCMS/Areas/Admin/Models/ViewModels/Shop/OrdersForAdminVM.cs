ea257d8a5be5cfd11bf8ab02e88c8bdb95a687f3
Robert Chou <robertchou91@gmail.com>
2019-07-22 08:24:34 +0800

    Creating the OrdersVM.

---
+++ ArtCMS/Areas/Admin/Models/ViewModels/Shop/OrdersForAdminVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtCMS.Areas.Admin.Models.ViewModels.Shop
+{
+    public class OrdersForAdminVM
+    {
+        public int OrderNumber { get; set; }
+        public string Username { get; set; }
+        public decimal total { get; set; }
+        public Dictionary<string, int> ProductsAndQty { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
